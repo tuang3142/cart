ebaa5e2b82675d516b39972adef148f5a12d58b5
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-05-26 19:34:33 +0700

    General cases to validate products
    - but they are kinda redundant. should be deleted later

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be possitive" do
+    product = Product.new(
+      title: "title",
+      description: "desc",
+      image_url: "url.jpg"
+    )
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      price: 1,
+      image_url: image_url
+    )
+  end
+
+  test "image url" do
+    good_urls = %w[fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad_urls = %w[fred.doc fred.gif/more fred.gif.more]
+
+    good_urls.each do |url|
+      assert new_product(url).valid?, "#{url} should be valid"
+    end
+
+    bad_urls.each do |url|
+      assert new_product(url).invalid?, "#{url} should be invalid"
+    end
+  end
 end
