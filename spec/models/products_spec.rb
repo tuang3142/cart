31c87c8686d1d0270d4d75852a6101bd31468c30
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-01 19:34:02 +0700

    simple rspec to make sure it works

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Products, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+  describe ".title" do
+    it "should have at least 10 characters" do
+      product = create(:product)
+      expect(product).to be_valid
+    end
+  end
 end
