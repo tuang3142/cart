d6d17dea0754b06eab17b394452f4a2dd58f7c3d
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-05 14:39:18 +0700

    Add lintstaged

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :product do
     sequence(:title) { |n| "012346789#{n}" }
     description  { "desc" }
-    sequence(:image_url) { |n| "#{n}.jpeg" }
+    sequence(:image_url) { "0.jpeg" }
     price { 1 }
   end
 end
