84f653e63222e28f73761d3f3f26ee9fc4440acb
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-08 08:31:47 +0700

    tidy with double quote

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "store#index", as: "store_index"
+
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
   resources :products
 end
