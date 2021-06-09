4d24b03d876bbd3b3edce9176edb072e80449d95
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-09 10:35:44 +0700

    config to use chrome driver

--- spec/features/add_line_item_to_cart_spec.rb
+++ spec/features/add_line_item_to_cart_spec.rb
@@ -2,10 +2,10 @@
 
 require "rails_helper"
 
-describe "add line_item to cart", type: :feature do
+describe "add line_item to cart", type: :feature, chrome_headless: true do
   it "redirects" do
     ruby_book, docker_book = create_list(:product, 2)
-    visit store_index_url
+    visit store_index_path
     expect(page).to have_content ruby_book.title
 
     within("main ul.catalog li", text: ruby_book.title) do
@@ -19,8 +19,7 @@ describe "add line_item to cart", type: :feature do
     expect(page).to have_css("h2", text: "Your Pragmatic Cart")
     expect(page).to have_css("li", text: ruby_book.title)
 
-    # should be go back wtf
-    visit store_index_url
+    go_back
 
     within("main ul.catalog li", text: docker_book.title) do
       expect { click_on "Add to Cart" }
