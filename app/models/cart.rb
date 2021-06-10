1e974d50bd8b22cdcc55073195541950ac542e3d
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-10 20:02:28 +0700

    create add_product for cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,13 @@
 
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.new(product_id: product.id)
+    end
+  end
 end
