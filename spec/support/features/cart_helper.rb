40cf963178a4abc1956484d2d4ff5bf99394b156
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-26 10:21:29 +0700

    Use cart helpers as module

--- spec/support/features/cart_helper.rb
+++ spec/support/features/cart_helper.rb
@@ -1,5 +1,9 @@
-def add_to_cart(item)
-  within("main ul.catalog li", text: item.title) do
-    click_on "Add to Cart"
+# frozen_string_literal: true
+
+module Features
+  def add_to_cart(item)
+    within("main ul.catalog li", text: item.title) do
+      click_on "Add to Cart"
+    end
   end
 end
