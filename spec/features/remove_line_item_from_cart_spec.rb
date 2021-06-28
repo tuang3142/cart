5f1f2240305e8140a500ece1976787eb1f5c5f10
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-28 17:49:52 +0700

    Add button to remove item from cart

---
+++ spec/features/remove_line_item_from_cart_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "User removes a line item from cart", chrome_headless: true do
+  scenario "successfully" do
+    ruby_book, docker_book = create_list(:product, 2)
+
+    visit store_index_path
+
+    add_to_cart(ruby_book)
+    add_to_cart(docker_book)
+
+    remove_from_cart(ruby_book)
+    remove_from_cart(docker_book)
+
+    expect(page).not_to have_content("Your cart")
+  end
+end
--- spec/support/features/cart_helper.rb
+++ spec/support/features/cart_helper.rb
@@ -6,4 +6,25 @@ module Features
       click_on "Add to Cart"
     end
   end
+
+  def remove_from_cart(item)
+    within "#line_item#{item.id}" do
+      click_on "Remove"
+    end
+  end
+
+  def expect_cart_to_be_empty
+    expect(page).to have_css(
+      "aside#notice",
+      text: "Cart is currently empty"
+    )
+
+    expect(page).not_to have_css("div#cart")
+  end
+
+  # def expect_cart_to_have_item(item)
+  #   within "div#cart" do
+  #     expect(page).to have_css("td", text: 
+  #   end
+  # end
 end
