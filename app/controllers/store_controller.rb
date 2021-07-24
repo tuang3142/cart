bb07d82275c378e594e61ea2000e63907e855e43
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-07-24 10:22:26 +0700

    Axe counter exercise from main app

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,13 +2,10 @@
 
 class StoreController < ApplicationController
   include CurrentCart
-  include Counter
 
   before_action :find_or_create_cart, only: :index
-  before_action :count_visit, only: :index
 
   def index
     @products = Product.order(:title)
-    @counter = counter
   end
 end
